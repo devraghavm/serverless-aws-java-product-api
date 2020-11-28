67dcc5c8b24f41e63f24b8590b3c4e0ae3974166
Raghav Medapati <medapati.raghu@gmail.com>
2020-11-27 22:18:15 -0700

    first commit

---
+++ src/main/java/com/serverless/UpdateProductHandler.java
@@ -0,0 +1,52 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.serverless.dal.Product;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class UpdateProductHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+        try {
+            // get the 'pathParameters' from input
+            Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+            String productId = pathParameters.get("id");
+            // get the 'body' from input
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+
+            // create the Product object for post
+            Product product = new Product();
+            // product.setId(body.get("id").asText());
+            product.setName(body.get("name").asText());
+            product.setPrice((float) body.get("price").asDouble());
+            Product updatedResponse = product.update(productId, product);
+
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(updatedResponse)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+
+        } catch (Exception ex) {
+            logger.error("Error in saving product: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in saving product: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
+}
